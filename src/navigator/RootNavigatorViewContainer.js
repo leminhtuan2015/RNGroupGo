c9c2dea94c11fbc043f3ea9d59820305bba009a2
Admin <admin@LeMinhTuan.net.fpt>
2018-03-24 13:07:18 +0700

    fix_react_navigations

--- src/navigator/RootNavigatorViewContainer.js
+++ src/navigator/RootNavigatorViewContainer.js
@@ -1,18 +1,31 @@
 import React from 'react';
-import {addNavigationHelpers} from 'react-navigation';
-import {connect} from 'react-redux';
+import { addNavigationHelpers } from 'react-navigation';
+import { connect } from 'react-redux';
+
+import {
+    createReduxBoundAddListener,
+    createReactNavigationReduxMiddleware,
+} from 'react-navigation-redux-helpers';
 
 import RootNavigatorView from "./RootNavigatorView"
+
+const middleware = createReactNavigationReduxMiddleware(
+    "root",
+    state => state.nav,
+);
+
+const addListener = createReduxBoundAddListener("root");
 
 const mapStateToProps = (state) => ({
     nav: state.nav
 });
 
-const NavigatorViewHelper = ({dispatch, nav}) => (
+const NavigatorViewHelper = ({ dispatch, nav }) => (
     <RootNavigatorView
         navigation={addNavigationHelpers({
             dispatch,
-            state: nav
+            state: nav,
+            addListener
         })}
     />
 );
