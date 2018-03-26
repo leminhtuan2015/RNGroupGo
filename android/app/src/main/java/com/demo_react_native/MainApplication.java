d43af42c891e179a58f05838043c2300e7feacaa
leminhtuan2015 <tuan.le@fujitechjsc.com>
2018-03-26 23:05:02 +0700

    fix_bug_cannot_build_android

--- android/app/src/main/java/com/demo_react_native/MainApplication.java
+++ android/app/src/main/java/com/demo_react_native/MainApplication.java
@@ -2,6 +2,7 @@ package com.demo_react_native;
 
 import android.app.Application;
 
+import com.facebook.BuildConfig;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookSdk;
 import com.facebook.react.ReactApplication;
