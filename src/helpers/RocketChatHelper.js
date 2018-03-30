fad8ef61b34f884444471a94c2c5e1b6c459dbd3
tuan.le <tuan.le@fujitechjsc.com>
2018-03-30 23:31:28 +0700

    ok_rocket_chat

--- src/helpers/RocketChatHelper.js
+++ src/helpers/RocketChatHelper.js
@@ -1,16 +1,18 @@
 // import DDP from "react-ddp";
 // import Meteor from 'react-native-meteor';
+// import DDPClient from "ddp";
 import DDPClient from "ddp-client";
+
 
 const serverHost = "wss://open.rocket.chat/websocket";
 const authToken = "32mnBoMBAYXGU-YP94XZ6oNm01II6di2cSv6qGouRck";
 const subscribe = "GENERAL";
 
-
-
 class RocketChatHelper {
 
     static subscribe() {
+        process.nextTick = setImmediate
+        
         console.log("RocketChatHelper Initing")
         console.log("RocketChatHelper Connecting")
 
@@ -44,23 +46,19 @@ class RocketChatHelper {
                 'login',             // name of Meteor Method being called
                 [{ "resume": authToken }],            // parameters to send to Meteor Method
                 function (err, result) {   // callback which returns the method call results
-                    console.log('called function, result: ' + JSON.stringify(result));
+                    console.log('called login success: ' + JSON.stringify(result));
 
                     if (err) {
-                        console.log('called function, err: ' + JSON.stringify(err));
+                        console.log('called login err: ' + JSON.stringify(err));
                     } else {
-                        // ddpclient.subscribe("stream-room-messages", [subscribe, false], function () {
-                        //     console.log("Subscription Complete.\n");                            
-                        //     ddpclient.on("message", function (msg) {
-                        //         console.log("On message -----> : " + msg);
-                        //     });
-                        // })
+                        ddpclient.subscribe("stream-room-messages", [subscribe, false], function () {
+                            console.log("Subscription Complete.\n");                            
+                            ddpclient.on("message", function (msg) {
+                                console.log("On message -----> : " + msg);
+                            });
+                        })
                     }
                 });
-
-            ddpclient.on('message', function (msg) {
-                console.log("ddp message: " + msg);
-            });
         });
 
     }
